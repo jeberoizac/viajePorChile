9f877c7e89098790a0ef3244839c4c7e3e1dcf84
jeberoizac <148498847+jeberoizac@users.noreply.github.com>
2024-02-28 22:31:22 -0300

    primer commit

--- assets/js/scriptJQ.js
+++ assets/js/scriptJQ.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
   var hash = this.hash;
   $('html, body').animate({
   scrollTop: $(hash).offset().top
-  }, 2000, function(){
+  }, 1000, function(){
   window.location.hash = hash;
   });
   }
